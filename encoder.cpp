aff3d9f7cfefc8af0f48709a29ad6f29bcc7b265
42LayerToiletpaper <128383885+42LayerToiletpaper@users.noreply.github.com>
2023-04-13 13:44:39 +0200

    Add files via upload

---
+++ encoder.cpp
@@ -0,0 +1,53 @@
+//
+// Created by mo on 4/11/23.
+//
+
+#include "encoder.h"
+
+//#include <string>
+
+
+
+void Encoder::encodeString(const std::string &in, std::string &out) {
+    out.clear();
+    out.resize((in.size() / 3) * 4);
+    const char* inPtr = &in[0];
+    char* outPtr = &out[0];
+
+    const size_t loop= in.size() / 12;
+
+
+    for(int i = 0; i < loop; i++){
+        //everytime the last 4 out of 16 byte string data get lost through processing, therefore 12 byte jumps
+        simde__m128i data = simde_mm_load_si128((simde__m128i*) inPtr + i*12 );
+        data = simde_mm_shuffle_epi8(data, shuffleMask);
+        //by shuffling every 3 8bit ASCII Letters now take up 4 bytes, "ABC" gets shuffled to "CCBA" to match the 4 bytes of the Base64 Encoding, and deal with little Endianness.
+
+        data =   simde_mm_srli_epi32(data, 2 ) & mask1 |
+                        simde_mm_srli_epi32(data, 4 ) & mask2 |
+                        simde_mm_srli_epi32(data, 6 ) & mask3 |
+                        data & mask4;
+
+        data = simde_mm_shuffle_epi8(data, shuffleMask2);
+
+
+        simde__m128i bigLetterMask= simde_mm_cmplt_epi8(data, _mm_set1_epi8(26)); // (a < b) ? 0xFF : 0x00
+        simde__m128i allMask= bigLetterMask;
+        simde__m128i smallLetterMask = simde_mm_andnot_si128(allMask, simde_mm_cmplt_epi8(data,simde_mm_set1_epi8(52))); //not allMask and  b where b is 0xFF if binaries are smaller than 52
+        allMask |= smallLetterMask;
+        simde__m128i numberMask = simde_mm_andnot_si128(allMask, simde_mm_cmplt_epi8(data, simde_mm_set1_epi8(62)));
+        allMask |= numberMask;
+        simde__m128i plusMask = simde_mm_andnot_si128(allMask, simde_mm_cmplt_epi8(data, simde_mm_set1_epi8(63)));
+        allMask |= plusMask;
+        simde__m128i & slashAntiMask = allMask;
+
+        data =  bigLetterMask   & simde_mm_add_epi8(data, allA)|
+                smallLetterMask & simde_mm_add_epi8(data, alla)|
+                numberMask      & simde_mm_add_epi8(data, all0)|
+                plusMask        & allPlus                      |
+                simde_mm_andnot_si128(slashAntiMask, allSlash);
+
+        simde_mm_storeu_si128((simde__m128*) outPtr + i * 16, data);
+
+    }
+}
---
+++ encoder.h
@@ -0,0 +1,40 @@
+//
+// Created by mo on 4/11/23.
+//
+#include <string>
+#include </home/mo/IT/openMS/simde-master/simde/x86/sse.h>
+#include </home/mo/IT/openMS/simde-master/simde/x86/sse2.h>
+#include </home/mo/IT/openMS/simde-master/simde/x86/ssse3.h>
+#include </home/mo/IT/openMS/simde-master/simde/x86/avx512.h>
+#ifndef SSEPLAYGROUND_ENCODER_H
+#define SSEPLAYGROUND_ENCODER_H
+
+
+class Encoder {
+
+public:
+
+    Encoder() = default;
+
+    void encodeString(const std::string &in, std::string &out);
+
+private:
+    const simde__m128i mask1 = simde_mm_set1_epi32(0x3F000000);//00111111 00000000 00000000 00000000
+    const simde__m128i mask2 = simde_mm_set1_epi32(0x003F0000);//00000000 00111111 00000000 00000000
+    const simde__m128i mask3 = simde_mm_set1_epi32(0x00003F00);//00000000 00000000 00111111 00000000
+    const simde__m128i mask4 = simde_mm_set1_epi32(0x0000003F);//00000000 00000000 00000000 00111111
+
+
+
+    const simde__m128i allA = simde_mm_set1_epi8('A');
+    const simde__m128i alla = simde_mm_set1_epi8('a' - 26);
+    const simde__m128i all0 = simde_mm_set1_epi8('0' - 52);
+    const simde__m128i allPlus = simde_mm_set1_epi8('+');
+    const simde__m128i allSlash = simde_mm_set1_epi8('/');
+
+    const simde__m128i shuffleMask = simde_mm_setr_epi8(2, 2, 1, 0, 5, 5, 4, 3, 8, 8, 7, 6, 11, 11, 10, 9);
+    const simde__m128i shuffleMask2 = simde_mm_setr_epi8(3,2,1,0,7,6,5,4,11,10,9,8,15,14,13,12);
+    //simde__m512i bigLetterMask, smallLetterMask, numberMask, plusMask, allMask;
+};
+
+#endif //SSEPLAYGROUND_ENCODER_H
