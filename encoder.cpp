9e921226cb95858e81b4d6bf01d1533673e04849
Moritz Berger <morib70@nero.imp.fu-berlin.de>
2023-04-18 16:31:39 +0200

    fixed loop calculation (all tests worked); added padding aufter loop

--- encoder.cpp
+++ encoder.cpp
@@ -16,12 +16,12 @@ void Encoder::encodeString(const std::string &in, std::string &out) {
     //char c = * (inPtr +12);
 
      
-    const size_t loop = in.size()%12 ? (in.size() / 12) : (in.size() / 12) +1;
+    const size_t loop = !(in.size()%12) ? (in.size() / 12) : (in.size() / 12) +1;
 
 
 
-
-
+    
+    uint padding = 3-(in.size()%3);
 
 
     for(int i = 1; i < loop; i++){
@@ -57,42 +57,8 @@ void Encoder::encodeString(const std::string &in, std::string &out) {
         simde_mm_storeu_si128((simde__m128*) (outPtr + i * 16), data);
 
     }
-
-/*
-
-    //durchlauf der immer aufgerufen wird. (es werden die letzten in.size()%12 zeichen encoded)
-
-            //everytime the last 4 out of 16 byte string data get lost through processing, therefore 12 byte jumps
-        simde__m128i data = simde_mm_lddqu_si128((simde__m128i*) (inPtr + (in.size()%12)) );
-        data = simde_mm_shuffle_epi8(data, shuffleMask);
-        //by shuffling every 3 8bit ASCII Letters now take up 4 bytes, "ABC" gets shuffled to "CCBA" to match the 4 bytes of the Base64 Encoding, and deal with little Endianness.
-
-        data =   simde_mm_srli_epi32(data, 2 ) & mask1 |
-                        simde_mm_srli_epi32(data, 4 ) & mask2 |
-                        simde_mm_srli_epi32(data, 6 ) & mask3 |
-                        data & mask4;
-
-        data = simde_mm_shuffle_epi8(data, shuffleMask2);
-
-
-        simde__m128i bigLetterMask= simde_mm_cmplt_epi8(data, _mm_set1_epi8(26)); // (a < b) ? 0xFF : 0x00
-        simde__m128i allMask= bigLetterMask;
-        simde__m128i smallLetterMask = simde_mm_andnot_si128(allMask, simde_mm_cmplt_epi8(data,simde_mm_set1_epi8(52))); //not allMask and  b where b is 0xFF if binaries are smaller than 52
-        allMask |= smallLetterMask;
-        simde__m128i numberMask = simde_mm_andnot_si128(allMask, simde_mm_cmplt_epi8(data, simde_mm_set1_epi8(62)));
-        allMask |= numberMask;
-        simde__m128i plusMask = simde_mm_andnot_si128(allMask, simde_mm_cmplt_epi8(data, simde_mm_set1_epi8(63)));
-        allMask |= plusMask;
-        simde__m128i & slashAntiMask = allMask;
-
-        data =  bigLetterMask   & simde_mm_add_epi8(data, allA)|
-                smallLetterMask & simde_mm_add_epi8(data, alla)|
-                numberMask      & simde_mm_add_epi8(data, all0)|
-                plusMask        & allPlus                      |
-                simde_mm_andnot_si128(slashAntiMask, allSlash);
-
-        simde_mm_storeu_si128((simde__m128*) (outPtr + (in.size()%12) * 16), data);
-
-*/
+    if(padding!=3){
+    out.resize(out.size()+padding, '=');
+    }
     
 }
